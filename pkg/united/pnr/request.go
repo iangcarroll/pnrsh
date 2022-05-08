adf97ca21102915a07194cccb8d9d0606dc31f32
Ian Carroll <ian@ian.sh>
2022-05-08 03:15:48 +0100

    Fix headers.

--- pkg/united/pnr/request.go
+++ pkg/united/pnr/request.go
@@ -17,9 +17,12 @@ const (
 
 var (
 	requestHeaders = map[string]string{
-		"Accept":          "*/*",
+		"Accept":          "application/json",
+		"Accept-Language": "en-US",
 		"Content-Type":    "application/json",
-		"Accept-Language": "en-US,en;q=0.9",
+		"DNT":             "1",
+		"Origin":          "https://www.united.com",
+		"Referer":         "https://www.united.com/en/us/awardaccelerator",
 		"User-Agent":      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
 	}
 
